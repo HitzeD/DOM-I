533fe812d09f0ee288d909e882ed6f5a8c7d18c2
HitzeD <d-hitze@comcast.net>
2019-04-08 15:24:04 -0500

    periodic save

--- js/index.js
+++ js/index.js
@@ -39,4 +39,57 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+// Self additions -----------------------------------------------
+
+// nav bar
+const aTags = document.querySelectorAll("nav a");
+aTags[0].innerHTML = siteContent["nav"]['nav-item-1'];
+aTags[1].innerHTML = siteContent["nav"]['nav-item-2'];
+aTags[2].innerHTML = siteContent["nav"]['nav-item-3'];
+aTags[3].innerHTML = siteContent["nav"]['nav-item-4'];
+aTags[4].innerHTML = siteContent["nav"]['nav-item-5'];
+aTags[5].innerHTML = siteContent["nav"]['nav-item-6'];
+
+
+// header IMG
+
+let headImg = document.querySelector("#cta-img");
+headImg.setAttribute('src' , siteContent["cta"]["img-src"]);
+
+// CTA
+
+const titl = document.querySelector("h1");
+const btn = document.querySelector("button");
+titl.innerHTML = siteContent["cta"]["h1"];
+btn.innerHTML = siteContent["cta"]["button"];
+
+// MID SECTION  --------------------------------
+
+const topCont = document.querySelectorAll(".top-content div");
+topCont[0].querySelector("h4").innerHTML = siteContent["main-content"]["features-h4"];
+topCont[0].querySelector("p").innerHTML = siteContent["main-content"]["features-content"];
+
+topCont[1].querySelector("h4").innerHTML = siteContent["main-content"]["about-h4"];
+topCont[1].querySelector("p").innerHTML = siteContent["main-content"]["about-content"];
+
+// Mid Image
+
+const midImg = document.querySelector("#middle-img");
+midImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+// bottom content
+
+const botitl = document.querySelectorAll(".bottom-content .text-content h4");
+const bopara = document.querySelectorAll(".bottom-content .text-content p");
+botitl[0].innerHTML = siteContent["main-content"]["services-h4"];
+botitl[1].innerHTML = siteContent["main-content"]["product-h4"];
+botitl[2].innerHTML = siteContent["main-content"]["vision-h4"];
+
+bopara[0].innerHTML = siteContent["main-content"]["services-content"];
+bopara[1].innerHTML = siteContent["main-content"]["product-content"];
+bopara[2].innerHTML = siteContent["main-content"]["vision-content"];
+
+// Contact Section
+
